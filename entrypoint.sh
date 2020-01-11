be59c041ecf64f6bc9cb51a74c587390b3c7a007
Juan Eugenio Abadie <juaneabadie@gmail.com>
2020-01-11 19:10:31 -0300

    Show build line

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,12 +53,14 @@ build_image() {
   fi
 
   # build image using cache
+  set -x
   docker build \
     $cache_from \
     --tag "$(_get_full_image_name)":${INPUT_IMAGE_TAG} \
     --file ${INPUT_CONTEXT}/${INPUT_DOCKERFILE} \
     ${INPUT_BUILD_EXTRA_ARGS} \
     ${INPUT_CONTEXT} | tee "$BUILD_LOG"
+  set +x
 }
 
 push_git_tag() {
