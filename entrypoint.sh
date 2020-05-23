afff327d8f171dc6d028f7697f5ed8e71c3457d3
Juan Eugenio Abadie <juaneabadie@gmail.com>
2020-05-22 22:59:31 -0300

    Allow word splitting of $cache_from

--- entrypoint.sh
+++ entrypoint.sh
@@ -198,10 +198,9 @@ build_image() {
   # build image using cache
   set -o pipefail
   set -x
-  # Disable SC2086 because we actually want word splitting to nothing
   # shellcheck disable=SC2086
   docker build \
-    "$cache_from" \
+    $cache_from \
     --tag "$DUMMY_IMAGE_NAME" \
     --file "${INPUT_CONTEXT}"/"${INPUT_DOCKERFILE}" \
     ${INPUT_BUILD_EXTRA_ARGS} \
