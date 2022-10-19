b45cf381b6bf0a52bfc30fb4e080e5d17a74c175
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-10-19 23:56:25 +0200

    Allow adding multiple values to same key in BUILD_EXTRA_ARGS

--- docker-build.sh
+++ docker-build.sh
@@ -210,16 +210,16 @@ _parse_extra_args() {
   fi
 
   # json
-  declare -ga extra_args
+  declare -ga extra_args=()
   local key
   local value
   while read -r key; do
-    value=$(_remove_quotes "$(jq ".$key" <<<"${INPUT_BUILD_EXTRA_ARGS}")")
-    key=$(_remove_quotes "$key")
-    extra_args+=("$key")
-    extra_args+=("${value//\\n/
+    for value in $(jq --raw-output "[.\"$key\"] | flatten[]" <<<"${INPUT_BUILD_EXTRA_ARGS}"); do
+      extra_args+=("$key")
+      extra_args+=("${value//\\n/
 }")
-  done < <(jq "keys[]" <<<"${INPUT_BUILD_EXTRA_ARGS}")
+    done
+  done < <(jq --raw-output "keys[]" <<<"${INPUT_BUILD_EXTRA_ARGS}")
   INPUT_BUILD_EXTRA_ARGS=""
 }
 
