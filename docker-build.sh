ec81c5227efa1a39374c3c6f5fb6fe983f1bb837
Joshua Van Deren <livetoscout@gmail.com>
2024-06-05 00:10:37 -0600

    Add star ^ and end $

--- docker-build.sh
+++ docker-build.sh
@@ -28,8 +28,8 @@ _is_new_github_registry() {
 _is_gcloud_artifact_registry() {
   # Docker repository: https://cloud.google.com/artifact-registry/docs/docker/names#docker-repo
   # Domain-scoped project: https://cloud.google.com/artifact-registry/docs/docker/names#domain
-  [[ "$INPUT_REGISTRY" =~ ([a-z0-9-]+)-docker.pkg.dev\/([a-z0-9-]+)\/([a-z0-9-]+) ]] \
-  || [[ "$INPUT_REGISTRY" =~ ([a-z0-9-]+)-docker.pkg.dev\/([a-z0-9-.]+)\/([a-z0-9-]+)\/([a-z0-9-]+) ]]
+  [[ "$INPUT_REGISTRY" =~ ^([a-z0-9-]+)-docker.pkg.dev\/([a-z0-9-]+)\/([a-z0-9-]+)$ ]] \
+  || [[ "$INPUT_REGISTRY" =~ ^([a-z0-9-]+)-docker.pkg.dev\/([a-z0-9-.]+)\/([a-z0-9-]+)\/([a-z0-9-]+)$ ]]
 }
 
 _is_gcloud_container_registry() {
